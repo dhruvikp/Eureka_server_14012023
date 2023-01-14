a447f8d40b0faaa0a8d3fbdb9e0cc0974d135fec
Dhruvik <dhruvik@simplilearn.com>
2023-01-14 12:41:24 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/EurekaServer14012023Application.java
@@ -0,0 +1,15 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer14012023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer14012023Application.class, args);
+	}
+
+}
